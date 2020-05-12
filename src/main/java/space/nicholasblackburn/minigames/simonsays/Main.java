d165256fe72416cc5325d9a18e0097ae22df7d40
nicholasblackburn1 <nickblakcburn02@gmail.com>
2020-05-12 09:55:46 -0400

    added reading from config file

--- src/main/java/space/nicholasblackburn/minigames/simonsays/Main.java
+++ src/main/java/space/nicholasblackburn/minigames/simonsays/Main.java
@@ -1,55 +1,74 @@
 package space.nicholasblackburn.minigames.simonsays;
 
+import java.io.File;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-
-
 
 public class Main extends JavaPlugin {
 
+    java.util.logging.Logger logger = Bukkit.getLogger();
+
+    File ymlFile = new File("config.yml");
+    FileConfiguration config = this.getConfig();
+
     @Override
-    public void onLoad(){
-        this.getConfig().options().copyDefaults(true);
+    public void onLoad() {
+        createConfig();
         
-        // saves config on run
-        this.getConfig().options().copyDefaults();
-        saveConfig();
-
-        System.out.println("Config random order/n"+readRandom());
-        System.out.println("Spawn"+readSpawnPosX()+readSpawnPosY()+readSpawnPosZ());
-
-
-    }
-
-    @Override 
-    public void onEnable(){
 
     }
 
     @Override
-    public void onDisable(){
+    public void onEnable() {
+        dumpConfig();
 
+        
     }
 
+    @Override
+    public void onDisable() {
+
+    }
 
     /**
-     * This Section IS where i define all the 
+     * This Section IS where i define all the
+     * 
      * @implNote spawns x,y,z
      * @return read random bool
      */
 
+    public void createConfig() {
+        if (!config.getBoolean("created")) {
+            config.addDefault("created", true);
+        }
+
+        if (config.getBoolean("created")) {
+            System.out.println("Config.yml created");
+            saveConfig();
+        }
+    }
+
+    public void dumpConfig(){
+        
+        logger.warning("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        logger.warning("Config random order\n" + readRandom());
+        logger.warning("Spawn\n" + "X"+readSpawnPosX()+"\n"+"Y"+ readSpawnPosY()+"\n"+"Z"+ readSpawnPosZ()+"\n");
+        logger.warning("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    }
     public Boolean readRandom(){
-        return this.getConfig().getBoolean("Random");
+        return config.getBoolean("Random");
     }
 
     public Integer readSpawnPosX(){
-        return this.getConfig().getInt("Spawn.x");
+        return config.getInt("Spawn.PosX");
     }
-    
     public Integer readSpawnPosY(){
-        return this.getConfig().getInt("Spawn.y");
+        return config.getInt("Spawn.PosY");
     }
 
     public Integer readSpawnPosZ(){
-        return this.getConfig().getInt("Spawn.z");
+        return config.getInt("Spawn.PosZ");
     }
 }
